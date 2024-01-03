047cd87ebc4eafc11416a68fd2f5247e2ed28fdc
Barath-tech <barathsundar03@gmail.com>
2024-01-03 20:32:22 +0530

    Simple client server operation

--- EchoClient.java
+++ EchoClient.java
@@ -1,12 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class EchoClient {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException { {
         try{
             System.out.println("Client started");
             Socket soc=new Socket("localhost",8000);
+            BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the string:");
+            String str=input.readLine();
+            PrintWriter out=new PrintWriter(soc.getOutputStream(),true);
+            out.println(str);
+            BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
         }catch(Exception e){
             e.printStackTrace();
         }
     }
+}
 }
--- EchoServer.java
+++ EchoServer.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +11,10 @@ public class EchoServer{
             ServerSocket ss=new ServerSocket(8000);
             Socket soc=ss.accept();
             System.out.println("Connection established");
+            BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String str=in.readLine();
+            PrintWriter out=new PrintWriter(soc.getOutputStream(),true);
+            out.println("Server says: " + str);
         }catch(Exception e){
             System.out.println("Error while connecting to Clients");
             e.printStackTrace();
